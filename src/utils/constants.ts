aa45ce15860ea503cdcf21f52f73b17073f1f561
Dennis Cope <dennisaaroncope@gmail.com>
2021-10-07 21:05:30 -0400

    Add env specific env files

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,13 +6,18 @@ export enum ENV {
 export enum Text {
   RECAPTCHA_SERVICE_FAIL = "An error occurred while attempting to contact reCAPTCHA services",
   RECAPTCHA_VERIFY_FAIL = "reCAPTCHA verification failed",
-  AWS_SERVICE_FAIL = "An error occurred while attempting to contact AWS services.",
+  RECAPTCHA_RESPONSE_FAIL = "reCAPTCHA returned an unsuccessful API response",
+  AWS_SERVICE_FAIL = "An error occurred while attempting to contact AWS services",
   REQUEST_FIELDS_MISSING = "Request is missing required field(s)",
   INDEX_GREETING = "Hello, world!",
-  DEVELOPMENT_SUCCESS = "This is a successful dummy response from the API in development mode.",
+  DEVELOPMENT_SUCCESS = "This is a successful dummy response from the API in development mode",
 }
 
 export enum MimeTypes {
   JSON = "application/json",
   URL_ENCODED = "application/x-www-form-urlencoded",
+}
+
+export enum ExternalUrls {
+  RECAPTCHA_VERIFY = "https://www.google.com/recaptcha/api/siteverify",
 }
--- src/utils/requestLibrary.ts
+++ src/utils/requestLibrary.ts
@@ -1,9 +1,9 @@
 import type { Response } from "node-fetch";
 import type { SendEmailCommandInput } from "@aws-sdk/client-ses";
-import fetch, { Response as FetchResponse } from "node-fetch";
+import fetch, { Response as ResponseObject } from "node-fetch";
 import { SendEmailCommand } from "@aws-sdk/client-ses";
 import { sesClient } from "../libs/sesClient.js";
-import { MimeTypes } from "./constants.js";
+import { MimeTypes, ExternalUrls } from "./constants.js";
 
 /**
  * Generic request handler
@@ -12,7 +12,7 @@ const handleRequest = async (f: () => Promise<Response>): Promise<Response> => {
   try {
     return await f();
   } catch (e: unknown) {
-    return FetchResponse.error();
+    return ResponseObject.error();
   }
 };
 
@@ -26,7 +26,7 @@ export const verifyRecaptcha = (token: string): Promise<Response> => {
   });
 
   const request = () =>
-    fetch("https://www.google.com/recaptcha/api/siteverify", {
+    fetch(ExternalUrls.RECAPTCHA_VERIFY, {
       method: "POST",
       body: body.toString(),
       headers: { "Content-Type": MimeTypes.URL_ENCODED },
@@ -43,8 +43,8 @@ export const sendAWSEmail = async (
 ): Promise<Response> => {
   try {
     const data = await sesClient.send(new SendEmailCommand(params));
-    return new FetchResponse(JSON.stringify(data));
+    return new ResponseObject(JSON.stringify(data));
   } catch (e: unknown) {
-    return FetchResponse.error();
+    return ResponseObject.error();
   }
 };
